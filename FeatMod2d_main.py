910a9c6ed122e8a741cae91695cc66af086fa10d
buckees <67809187+buckees@users.noreply.github.com>
2020-08-25 23:30:17 -0700

    Add random reflection test
    1. random reflection along the incident trajectory when hit the surface
    2. if not hit on surface, continue the advancement as a count of reflection
    3. count reflection and set max = 5
    4. if ptcl reaches or beyond top surface, remove the particle

--- FeatMod2d_main.py
+++ FeatMod2d_main.py
@@ -11,16 +11,14 @@ import matplotlib.cm as cm
 from FeatMod2d_ops import width, height, res_x, res_z, num_ptcl, threshold
 from FeatMod2d_mesh import MESHGRID
 from FeatMod2d_ptcl import PARTICLE
-from FeatMod2d_move import find_intersect_node
-from FeatMod2d_plot import plot_mesh, plot_itsct
 
-#threshold = 0.1
-#def update_surf(idx, mesh):
-#    if mesh.mat[idx] == 2:
-#            rnd = np.random.uniform(0.0, 1.0)
-#            if rnd < threshold:
-#                mesh.mat[idx] = 0
-
+def rotate_random(uvec):
+    theta = np.random.uniform(-math.pi/2.0, +math.pi/2.0)
+    theta = theta + math.pi
+    x1, z1 = uvec
+    x2 = math.cos(theta)*x1 - math.sin(theta)*z1
+    z2 = math.sin(theta)*x1 + math.cos(theta)*z1
+    return np.array([x2, z2])
 
 # create mesh
 mesh = MESHGRID(width, height, res_x, res_z)
@@ -38,29 +36,39 @@ for k in range(num_ptcl):
     Arp.init_vels('Normal')
 #    Arp.init_plot()
     record[k].append(Arp.posn.copy())
-    
-    for i in range(1000):
-        Arp.move_ptcl(delta_L)
-        Arp.posn[0] = Arp.posn[0] % mesh.width
-        record[k].append(Arp.posn.copy())
-        hit_mat, hit_idx = mesh.hit_check(Arp.posn)
-        if hit_mat:
-            break
-    rnd = np.random.uniform(0.0, 1.0)
-    if rnd < 0.5:
-#        reflection
-        vec_vel = Arp.vels
-        vec_vel = -vec_vel
-        vec_vel = rotate_random(vec_vel)
+
+    imove_ptcl, ireaction, iremove = 1, 0, 0
+    num_reflect = 0
+    while imove_ptcl == 1 and num_reflect < 5:
         for i in range(1000):
             Arp.move_ptcl(delta_L)
             Arp.posn[0] = Arp.posn[0] % mesh.width
             record[k].append(Arp.posn.copy())
+            if Arp.posn[1] >= mesh.height:
+                iremove = 1
+                break
             hit_mat, hit_idx = mesh.hit_check(Arp.posn)
             if hit_mat:
                 break
-    else:
+        if iremove == 1:
+            break
+        if not hit_mat:
+            num_reflect += 1
+            continue
+        rnd = np.random.uniform(0.0, 1.0)
+        if rnd < 0.5:
+    #        reflection
+            print(Arp.uvec)
+            Arp.uvec = rotate_random(Arp.uvec)
+            num_reflect += 1
+            print(Arp.uvec)
+        else:
+            imove_ptcl = 0
+            ireaction = 1
+            
+    if ireaction == 1:
         mesh.update_surf(hit_idx, threshold)
+    
     record[k] = np.array(record[k]).T
 
 #print(record[-1])
@@ -73,7 +81,7 @@ x = np.linspace(0.0, mesh.width, mesh.nx)
 z = np.linspace(0.0, mesh.height, mesh.nz) 
 X, Z = np.meshgrid(x, z) 
 ax.contourf(X, Z, mesh.mat.T, cmap = colMap, vmin = 0.2, extend='both')
-for i in range(20):
+for i in range(5):
     ax.plot(record[num_ptcl-i-1][0,:], record[num_ptcl-i-1][1,:], 
             marker = 'o', markersize=1, linestyle='None' )
 plt.show(fig)
