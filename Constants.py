77c9d0b4aa07c7b1c4284d801866b292b839065e
buckees <67809187+buckees@users.noreply.github.com>
2020-10-07 15:14:38 -0700

    Update Constants.py

--- Constants.py
+++ Constants.py
@@ -1,14 +1,17 @@
-# -*- coding: utf-8 -*-
-# This constants.py contains all constants which are called in PIC simulation
+"""Define all constants."""
 
-UNIT_CHARGE = 1.602e-19 # unit in C
-EON_MASS = 9.109e-31 # unit in kg
-AMU = 1.661e-27 # Atomic Mass Unit, unit in kg
-EPS0 = 8.854e-12 # permittivity vac. in F/m
-KB = 1.38064852e-23 # Boltzmann Constant, unit in m2*kg*s-2*K-1 or J/K
-EV2K = 11604.45 # 1 eV = 1.602 177 33 × 10-19 J = 11 604.45 K.
-TORR2PA = 133.3 # 1 Torr = 133.3 Pa
-EV2VEL = 6.0e5 # with 1 eV, electron has speed of 6.0e5 m/s, sqrt relationship
-VEL2EV = 1/EV2VEL # 1 m/s = 1/(6.0e5) eV, sqr relationship
-J2EV = 6.242e18 # 1 J = 6.242e18 eV
-EV2J = 1.6e-19 # 1 eV = 1.6e-19 J
+UNIT_CHARGE = 1.602e-19  # unit in C
+EON_MASS = 9.109e-31  # unit in kg
+AMU = 1.661e-27  # Atomic Mass Unit, unit in kg
+EPS0 = 8.854e-12  # permittivity vac. in F/m
+MU0 = 12.57e-7   # Vacuum permeability in H/m or in Tesla*m/A
+EV2K = 11604.45  # 1 eV = 1.602 177 33 × 10-19 J = 11 604.45 K.
+TORR2PA = 133.3  # 1 Torr = 133.3 Pa
+EV2VEL = 6.0e5  # with 1 eV, electron has speed of 6.0e5 m/s, sqrt relationship
+VEL2EV = 1/EV2VEL  # 1 m/s = 1/(6.0e5) eV, sqr relationship
+J2EV = 6.242e18  # 1 J = 6.242e18 eV
+EV2J = 1.6e-19  # 1 eV = 1.6e-19 J
+EV2K = 11600.0  # 1 eV = 1160 K, unit in K/eV
+K2EV = 1/EV2K  # 1 K = 8.621738e-5 eV, unit in eV/K
+KB = 1.38064852e-23  # Boltzmann Constant, unit in m2*kg*s-2*K-1 or J/K
+KB_EV = KB*EV2K  # Boltzmann Constant, unit in J/eV
