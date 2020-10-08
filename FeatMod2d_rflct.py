00c8c3d2a8ef9a082615a1a30285697c424f368c
buckees <67809187+buckees@users.noreply.github.com>
2020-10-08 10:00:09 -0700

    Add diff_rflct()
    1. Remove import math
    2. Replace math with numpy

--- FeatMod2d_rflct.py
+++ FeatMod2d_rflct.py
@@ -1,7 +1,7 @@
 """Feature Model 2D. Reflection."""
 
 import numpy as np
-from math import pi, sin, cos
+from scipy.stats import cosine
 
 
 class REFLECT(object):
@@ -11,20 +11,27 @@ class REFLECT(object):
         self.prob = prob  # reflection probability
         self.ptcl = ptcl  # particle species
         self.mat = mat  # materiasl which particle hits
+        self.theta = 0.0  # reflection angle wrt surface normal
 
     def calc_prob(self):
         """Calc probability."""
         if self.mat == 'PR':
             self.prob = 1.0
         elif self.mat == 'Si':
-            self.prob = 0.1
+            self.prob = 0.7
         return self.prob
 
     def rotate_random(self, uvec):
         """Rotate the direction randomly."""
-        theta = np.random.uniform(-pi/4.0, + pi/4.0)
-        theta = theta + pi
+        theta = np.random.uniform(-np.pi/4.0, + np.pi/4.0)
+        theta = theta + np.pi
         x1, z1 = uvec
-        x2 = cos(theta)*x1 - sin(theta)*z1
-        z2 = sin(theta)*x1 + cos(theta)*z1
+        x2 = np.cos(theta)*x1 - np.sin(theta)*z1
+        z2 = np.sin(theta)*x1 + np.cos(theta)*z1
         return np.array([x2, z2])
+
+    def diff_rflct(self, theta):
+        """Calc the diffusive refelection."""
+        self.theta  = cosine.rvs(size=1)
+        self.theta += theta
+        uvec = (np.cos(theta), np.sin(theta))
