ccacd5f2e26d0dbc37d747301b64f32bb655e18f
buckees <67809187+buckees@users.noreply.github.com>
2020-10-13 20:14:38 -0700

    Revise rotate_random to rand_rflct
    1. uniform sampling from (surf_norm - 90, surf_norm + 90)

--- FeatMod2d_rflct.py
+++ FeatMod2d_rflct.py
@@ -22,14 +22,11 @@ class REFLECT(object):
             self.prob = 0.7
         return self.prob
 
-    def rotate_random(uvec):
+    def rand_rflct(self):
         """Rotate the direction randomly."""
-        theta = np.random.uniform(-np.pi/4.0, + np.pi/4.0)
-        theta = theta + np.pi
-        x1, z1 = uvec
-        x2 = np.cos(theta)*x1 - np.sin(theta)*z1
-        z2 = np.sin(theta)*x1 + np.cos(theta)*z1
-        return np.array([x2, z2])
+        rtheta = np.random.uniform(-np.pi/2.0, np.pi/2.0)
+        rtheta += self.stheta
+        return np.array([np.cos(rtheta), np.sin(rtheta)])
     
     def spec_rflct(self, ivec):
         """
