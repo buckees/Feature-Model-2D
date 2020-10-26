43efa038c206042d1515f13689d50c0c4c2804e0
buckees <67809187+buckees@users.noreply.github.com>
2020-10-26 14:33:30 -0700

    Add mat1

--- FeatMod2d_mat.py
+++ FeatMod2d_mat.py
@@ -2,14 +2,28 @@
 Materials
 """
 
-materials = []
+mat0 = []
 m = [('SiO2', 1), 'rect', (0.0, 0.0, 100.0, 50.0)]
-materials.append(m)
+mat0.append(m)
 m = [('Si', 2),   'rect', (0.0, 50.0, 100.0, 300.0)]
-materials.append(m)
+mat0.append(m)
 m = [('PR', 3),   'rect', (0.0, 350.0, 30.0, 100.0)]
-materials.append(m)
+mat0.append(m)
 m = [('PR', 3),   'rect', (70.0, 350.0, 30.0, 100.0)]
-materials.append(m)
+mat0.append(m)
 m = [('Vac', 0),   'circ', (50.0, 350.0, 30.0)]
-materials.append(m)
+mat0.append(m)
+
+
+mat1 = []
+
+m = [('SiO2', 1), 'rect', (0.0, 0.0, 100.0, 50.0)]
+mat1.append(m)
+m = [('Si', 2),   'rect', (0.0, 50.0, 100.0, 300.0)]
+mat1.append(m)
+m = [('PR', 3),   'rect', (0.0, 350.0, 30.0, 50.0)]
+mat1.append(m)
+m = [('PR', 3),   'rect', (70.0, 350.0, 30.0, 50.0)]
+mat1.append(m)
+m = [('Vac', 0),   'circ', (50.0, 350.0, 30.0)]
+mat1.append(m)
