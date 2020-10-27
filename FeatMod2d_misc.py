ee1fc9b71ca094d8d3b003cbd4b9fd02f4def7c7
buckees <67809187+buckees@users.noreply.github.com>
2020-10-27 16:47:53 -0700

    Rename area to trgl_area

--- FeatMod2d_misc.py
+++ FeatMod2d_misc.py
@@ -6,7 +6,7 @@ Contain Misc. Functions
 # of triangle formed by (x1, y1),  
 # (x2, y2) and (x3, y3) 
   
-def area(x1, y1, x2, y2, x3, y3): 
+def trgl_area(x1, y1, x2, y2, x3, y3): 
   
     return abs((x1 * (y2 - y3) + x2 * (y3 - y1)  
                 + x3 * (y1 - y2)) / 2.0) 
@@ -15,19 +15,19 @@ def area(x1, y1, x2, y2, x3, y3):
 # A function to check whether point P(x, y) 
 # lies inside the triangle formed by  
 # A(x1, y1), B(x2, y2) and C(x3, y3)  
-def isInside(x1, y1, x2, y2, x3, y3, x, y): 
+def isInsideTrgl(x1, y1, x2, y2, x3, y3, x, y): 
   
     # Calculate area of triangle ABC 
-    A = area (x1, y1, x2, y2, x3, y3) 
+    A = trgl_area(x1, y1, x2, y2, x3, y3) 
   
     # Calculate area of triangle PBC  
-    A1 = area (x, y, x2, y2, x3, y3) 
+    A1 = trgl_area(x, y, x2, y2, x3, y3) 
       
     # Calculate area of triangle PAC  
-    A2 = area (x1, y1, x, y, x3, y3) 
+    A2 = trgl_area(x1, y1, x, y, x3, y3) 
       
     # Calculate area of triangle PAB  
-    A3 = area (x1, y1, x2, y2, x, y) 
+    A3 = trgl_area(x1, y1, x2, y2, x, y) 
       
     # Check if sum of A1, A2 and A3  
     # is same as A 
@@ -40,8 +40,9 @@ if __name__ == '__main__':
     # Driver program to test above function 
     # Let us check whether the point P(10, 15) 
     # lies inside the triangle formed by  
-    # A(0, 0), B(20, 0) and C(10, 30) 
-    if (isInside(0, 0, 20, 0, 10, 30, 10, 15)): 
+    tempx, tempz = 25.0, 400.0
+    
+    if (isInsideTrgl(30.0, 350.0, 30.0, 450.0, 0.0, 450.0, tempx, tempz)): 
         print('Inside') 
     else: 
         print('Not Inside')  
