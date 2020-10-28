065a701364e1107cfcdcc3a445ae4225f4be4bd3
buckees <67809187+buckees@users.noreply.github.com>
2020-10-28 10:33:10 -0700

    Update Diagnostics
    1. Plot the intermediate mesh during the run
    2. Move surf norm calc ahead of determing rflct or react

--- FeatMod2d_main.py
+++ FeatMod2d_main.py
@@ -21,17 +21,19 @@ mesh.find_surf()
 mesh.find_surf_vac()
 # mesh.plot()
 
-rec_traj, rec_surf, rec_mesh = [], [], []
-
 # delta_L = min(res_x, res_z)*0.5
 delta_L = min(res_x, res_z)
+# particle is imported from species
 # Arp = PARTICLE('Ar+', 'Ion',  32.0,     1)
-
 Arp_rflct = REFLECT()
+
+# init diagnostics
+rec_traj, rec_surf, rec_mesh = [], [], []
 
 for k in range(num_ptcl):
     if (k + 1) % int(num_ptcl/5) == 0:
-        print(k)
+        print('%d particles are launched!' % (k+1))
+        mesh.plot(dpi=300, fname='nptcl=%d.png' % (k+1))
         rec_mesh.append(deepcopy(mesh.mat))
     Arp.dead = 0
     Arp.init_posn(width, height)
@@ -40,7 +42,6 @@ for k in range(num_ptcl):
         rec_traj.append([])
         rec_traj[-1].append(Arp.posn.copy())
     Arp.init_uvec(['Uniform', -15.0, 15.0])
-#    Arp.init_plot()
 
     num_rflct = 0
 #    while imove_ptcl == 1 and num_rflct < 5:
@@ -49,8 +50,7 @@ for k in range(num_ptcl):
         Arp.move_ptcl(delta_L)
         # periodic b.c. at left and right bdry
         Arp.bdry_check(mesh.width, mesh.height, 'periodic')
-        # rec_traj ptcl trajectory
-        # rec_traj[k].append(Arp.posn.copy())
+        # check if the ptcl is dead
         if Arp.dead:
             # record ptcl posn when dead
             if k > num_ptcl - 20:
@@ -58,27 +58,24 @@ for k in range(num_ptcl):
             break
         hit_mat, hit_idx = mesh.hit_check(Arp.posn)
         if hit_mat:
-            # print(Arp.posn, hit_idx)
             # record the hit point
             if k > num_ptcl - 20:
                 rec_traj[-1].append(Arp.posn.copy())
             # at this position, th ptcl hits a mat
-            # decide wehter a reflection or reaction
             mat_name = mesh.mater[hit_mat]
+            # calc surf norm
+            Arp_rflct.svec, Arp_rflct.stheta = \
+                mesh.calc_surf_norm(hit_idx, radius=4, imode='Sum Vector')
+            # decide wehter a reflection or reaction
             rnd = np.random.uniform(0.0, 1.0)
             rflct = REFLECT(Arp.name, mat_name, 1.0)
             prob = rflct.calc_prob()
             if rnd < prob:
+                # check max rflct
                 if num_rflct > max_rflct:
                     Arp.dead = 1
-                    # record ptcl posn when dead
-                    if k > num_ptcl - 20:
-                        rec_traj[-1].append(Arp.posn.copy())
                     break
-                # call reflection
-                Arp_rflct.svec, Arp_rflct.stheta = \
-                    mesh.calc_surf_norm(hit_idx, radius=4, imode='Sum Vector')
-                # rec_surf.append([hit_idx, Arp_rflct.svec.copy()])
+                
                 # use only specular reflection
                 Arp.uvec = Arp_rflct.spec_rflct(Arp.uvec)
                 # Arp.uvec = Arp_rflct.rflct(Arp.uvec)
@@ -94,56 +91,61 @@ for k in range(num_ptcl):
                 Arp.dead = 1
         # check if the ptcl is dead
         if Arp.dead:
+            # record ptcl posn when dead
+            if k > num_ptcl - 20:
+                rec_traj[-1].append(Arp.posn.copy())
             break
+
     if k > num_ptcl - 20:
         rec_traj[-1] = np.array(rec_traj[-1]).T
 
-rec_surf = []
-for temp_idx in mesh.surf:
-    temp_svec, temp_stheta = mesh.calc_surf_norm(temp_idx, radius=4,
-                                                 imode="Sum Vector")
-    rec_surf.append([temp_idx, temp_svec])
 
-colMap = copy(cm.Accent)
-colMap.set_under(color='white')
+# rec_surf = []
+# for temp_idx in mesh.surf:
+#     temp_svec, temp_stheta = mesh.calc_surf_norm(temp_idx, radius=4,
+#                                                  imode='Sum Vector')
+#     rec_surf.append([temp_idx, temp_svec])
 
-def plot_traj(ax, traj):
-    # for i in range(10):
-    #     ax.plot(traj[num_ptcl - i - 1][0, :], traj[num_ptcl - i - 1][1, :],
-    #             marker='o', markersize=0.3, linestyle='-', linewidth=0.1)
-    for temp_ptcl in traj:
-        ax.plot(temp_ptcl[0, :], temp_ptcl[1, :],
-                marker='o', markersize=0.3, linestyle='-', linewidth=0.1)
+# colMap = copy(cm.Accent)
+# colMap.set_under(color='white')
 
-def plot_surf_norm(ax, posn, svec):
-    ax.quiver(posn[0], posn[1],
-              svec[0], svec[1], 
-              # scale=50, units='xy',
-              # headwidth=1, headlength=1, lw=0.01, edgecolors='k',
-              width=0.001)
+# def plot_traj(ax, traj):
+#     # for i in range(10):
+#     #     ax.plot(traj[num_ptcl - i - 1][0, :], traj[num_ptcl - i - 1][1, :],
+#     #             marker='o', markersize=0.3, linestyle='-', linewidth=0.1)
+#     for temp_ptcl in traj:
+#         ax.plot(temp_ptcl[0, :], temp_ptcl[1, :],
+#                 marker='o', markersize=0.3, linestyle='-', linewidth=0.1)
+
+# def plot_surf_norm(ax, posn, svec):
+#     ax.quiver(posn[0], posn[1],
+#               svec[0], svec[1], 
+#               # scale=50, units='xy',
+#               # headwidth=1, headlength=1, lw=0.01, edgecolors='k',
+#               width=0.001)
 
 
-def plot_mesh(mat, ith):
-    fig, axes = plt.subplots(1, 2, figsize=(16, 8),
-                             constrained_layout=True)
+# def plot_mesh(mat, ith):
+#     fig, axes = plt.subplots(1, 2, figsize=(16, 8),
+#                              constrained_layout=True)
     
-    ax = axes[0]
-    ax.contourf(mesh.x, mesh.z, mat, cmap=colMap, vmin=0.2, extend='both')
-    ax.set_xlim(0.0, mesh.width)
-    ax.set_ylim(0.0, mesh.height)
-    plot_traj(ax, rec_traj)
+#     ax = axes[0]
+#     ax.contourf(mesh.x, mesh.z, mat, cmap=colMap, vmin=0.2, extend='both')
+#     ax.set_xlim(0.0, mesh.width)
+#     ax.set_ylim(0.0, mesh.height)
+#     plot_traj(ax, rec_traj)
     
-    ax = axes[1]
-    ax.scatter(mesh.x, mesh.z, c=mat, s=1, cmap=colMap, vmin=0.2)
-    ax.set_xlim(0.0, mesh.width)
-    ax.set_ylim(0.0, mesh.height)
-    plot_traj(ax, rec_traj)
-    for item in rec_surf:
-        temp_idx, temp_svec = item
-        temp_posn = np.array([mesh.x[temp_idx], mesh.z[temp_idx]])
-        plot_surf_norm(ax, temp_posn, temp_svec)
+#     ax = axes[1]
+#     ax.scatter(mesh.x, mesh.z, c=mat, s=1, cmap=colMap, vmin=0.2)
+#     ax.set_xlim(0.0, mesh.width)
+#     ax.set_ylim(0.0, mesh.height)
+#     plot_traj(ax, rec_traj)
+#     for item in rec_surf:
+#         temp_idx, temp_svec = item
+#         temp_posn = np.array([mesh.x[temp_idx], mesh.z[temp_idx]])
+#         plot_surf_norm(ax, temp_posn, temp_svec)
     
-    fig.savefig('mat_%d.png' % ith, dpi=600)
+#     fig.savefig('mat_%d.png' % ith, dpi=300)
 
-for ith, mat in enumerate(rec_mesh):
-    plot_mesh(mat, ith+1)
+# for ith, mat in enumerate(rec_mesh):
+#     plot_mesh(mat, ith+1)
