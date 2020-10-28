854f288a15c1c01bbf007fb4bce9bee7ad32199c
buckees <67809187+buckees@users.noreply.github.com>
2020-10-27 17:05:18 -0700

    Update width and height

--- FeatMod2d_ops.py
+++ FeatMod2d_ops.py
@@ -1,8 +1,8 @@
 """Feature Model 2D. Operation parameters."""
 
 # mesh informaiton
-width, height = 100.0, 500.0  # nm
-res_x, res_z = 2.0, 2.0  # nm
+width, height = 200.0, 300.0  # nm
+res_x, res_z = 1.0, 1.0  # nm
 
 # particle information
 num_ptcl = 3000
