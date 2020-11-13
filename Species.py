f82881e6c34d3078eca7277b7c902d749bc9b8d8
buckees <67809187+buckees@users.noreply.github.com>
2020-11-13 14:53:07 -0800

    Delete objects and the species database becomes independent

--- Species.py
+++ Species.py
@@ -4,14 +4,6 @@ Store species list.
 Any species/particles used in the feature model should be selected from here.
 """
 
-from FeatMod2d_ptcl import PARTICLE
-
-# Ar = PARTICLE('Ar', 'Neut',  32.0,     0)
-# Arp = PARTICLE('Ar+', 'Ion',  32.0,     1)
-# Cl2 = PARTICLE('Cl2', 'Neut',  70.0,     0)
-# Cl = PARTICLE('Cl', 'Neut',  35.0,     0)
-# Clp = PARTICLE('Cl+', 'Ion',  35.0,     1)
-
 # name, ptype, mass, charge
 Ar = {'name':'Ar', 'ptype':'Neut', 'mass':32.0, 'charge':0}
 Arp = {'name':'Ar+', 'ptype':'Ion', 'mass':32.0, 'charge':1}
