bd002d021c75468d3bfc527e1cab4931fb8f2d48
buckees <67809187+buckees@users.noreply.github.com>
2020-11-13 14:52:11 -0800

    Replace PARTICLE objects with dict to store the pre-defined species

--- Species.py
+++ Species.py
@@ -6,11 +6,18 @@ Any species/particles used in the feature model should be selected from here.
 
 from FeatMod2d_ptcl import PARTICLE
 
-Ar = PARTICLE('Ar', 'Neut',  32.0,     0)
-Arp = PARTICLE('Ar+', 'Ion',  32.0,     1)
-Cl2 = PARTICLE('Cl2', 'Neut',  70.0,     0)
-Cl = PARTICLE('Cl', 'Neut',  35.0,     0)
-Clp = PARTICLE('Cl+', 'Ion',  35.0,     1)
+# Ar = PARTICLE('Ar', 'Neut',  32.0,     0)
+# Arp = PARTICLE('Ar+', 'Ion',  32.0,     1)
+# Cl2 = PARTICLE('Cl2', 'Neut',  70.0,     0)
+# Cl = PARTICLE('Cl', 'Neut',  35.0,     0)
+# Clp = PARTICLE('Cl+', 'Ion',  35.0,     1)
+
+# name, ptype, mass, charge
+Ar = {'name':'Ar', 'ptype':'Neut', 'mass':32.0, 'charge':0}
+Arp = {'name':'Ar+', 'ptype':'Ion', 'mass':32.0, 'charge':1}
+Cl2 = {'name':'Cl2', 'ptype':'Neut', 'mass':70.0, 'charge':0}
+Cl = {'name':'Cl', 'ptype':'Neut', 'mass':35.0, 'charge':0}
+Clp = {'name':'Cl+', 'ptype':'Ion', 'mass':35.0, 'charge':1}
 
 sp_full_list = {'Ar': Ar,
                 'Ar+': Arp,
