fa641f4470099419c2c25424b9014d5753ed42f8
buckees <67809187+buckees@users.noreply.github.com>
2020-11-13 15:06:45 -0800

    Update Species.py

--- Species.py
+++ Species.py
@@ -10,11 +10,11 @@ Arp = {'name':'Ar+', 'ptype':'Ion', 'mass':32.0, 'charge':1}
 Cl2 = {'name':'Cl2', 'ptype':'Neut', 'mass':70.0, 'charge':0}
 Cl = {'name':'Cl', 'ptype':'Neut', 'mass':35.0, 'charge':0}
 Clp = {'name':'Cl+', 'ptype':'Ion', 'mass':35.0, 'charge':1}
+Si = {'name':'Si', 'ptype':'Neut', 'mass':28.0, 'charge':0}
 
 sp_full_list = {'Ar': Ar,
                 'Ar+': Arp,
                 'Cl2': Cl2,
                 'Cl': Cl,
-                'Cl+': Clp}
-
-Si = PARTICLE('Si', 'Neut',  28.0,     0)
+                'Cl+': Clp,
+                'Si': Si}
