b7c1ebee08d27569cb7f598db6d9fa5117f4c311
buckees <67809187+buckees@users.noreply.github.com>
2020-11-13 16:12:45 -0800

    Rename most var

--- FeatMod2d_readin.py
+++ FeatMod2d_readin.py
@@ -3,18 +3,27 @@
 import yaml
 
 yaml_flux = open("flux.yaml", 'r')
-temp = yaml.load(yaml_flux, Loader=yaml.FullLoader)
-print(temp)
+# temp = yaml.load(yaml_flux, Loader=yaml.FullLoader)
+temp = yaml.safe_load(yaml_flux)
 
-species = temp['Species']
-flux = temp['Flux']
+sp_name = temp['Species']
+sp_flux = temp['Flux']
+sp_flux.update((k, float(v)) for k, v in sp_flux.items())
 
-print(species)
-print(flux)
 
 from Species import sp_full_list
 
-sp_run_list = {k:sp_full_list[k] for k in species}
-print(sp_run_list)
+sp_run_list = {k:sp_full_list[k] for k in sp_name}
+# print(species)
+# print(flux)
+# print(sp_run_list)
+
+
+sp_weight = [sp_flux[k] for k in sp_name]
+
+if __name__ == '__main__':
+    from random import choices
+    sp_temp = choices(sp_name, weights=sp_weight, k=1)
+    print(*sp_temp)
     
 
